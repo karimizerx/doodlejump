e96746fb33c7537a22e5a5320a33fe08c2bf4ef4
Ayman <ayman.khemakhem@etu.u-paris.fr>
2023-01-26 17:22:23 +0100

    création de la classe abstraite GameObject et la classe Plateforme

---
+++ src/GameObjects/Plateforme.java
@@ -0,0 +1,9 @@
+package src.GameObjects;
+
+public class Plateforme extends GameObject{
+
+    public Plateforme(double x, double y, double w, double h, double dx, double dy) {
+        super(x, y, 50, 10, dx, dy);
+    }
+    
+}
