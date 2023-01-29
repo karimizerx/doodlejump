cd4c2c819caa2c18bd24f5f9b13fa4f8cc910b38
laidouni <mohamed.laidouni@etu.u-paris.fr>
2023-01-29 21:20:59 +0100

    Reglage des pb de version java (instance of)

--- src/gameobjects/Personnage.java
+++ src/gameobjects/Personnage.java
@@ -1,88 +1,80 @@
 package gameobjects;
 
+public class Personnage extends GameObject implements Moveable {
 
-public class Personnage extends GameObject implements Moveable{
-    
-    private double dx,dy; 
-    public Personnage(double x, double y, double w, double h,double dy,double dy_i) {
+    private double dx, dy;
+
+    public Personnage(double x, double y, double w, double h, double dy, double dy_i) {
         super(x, y, w, h);
-        this.dy=dy;
+        this.dy = dy;
     }
-    
 
     @Override
     public void move(double deltaT) {
         // partie gravite
-        double g=9.81;
-        double newX,newY;
-        dy= dy>-g? (-g*deltaT+dy) :-g ;//chute libre ;
-        
-        this.setY((-g/2)*(deltaT*deltaT)+dy*deltaT+this.getY());
+        double g = 9.81;
+        double newX, newY;
+        dy = dy > -g ? (-g * deltaT + dy) : -g;// chute libre ;
 
-        //partie horizental
-        newX=dx*deltaT+this.getX();
+        this.setY((-g / 2) * (deltaT * deltaT) + dy * deltaT + this.getY());
+
+        // partie horizental
+        newX = dx * deltaT + this.getX();
         // if(newX+this.getHeight()/2>)
         this.setX(newX);
 
     }
 
     public boolean collides(GameObject go) {
-        double epsilone=0.0; // marge d'erreur qu'on se donne
-        boolean val=false;
-        if(go instanceof Items item){
-            val =(// on test si ils se chevauchent horizentalement  
-                    (this.getX() >=item.getX() && this.getX()<= item.getX()+item.getWidth())
+        double epsilone = 0.0; // marge d'erreur qu'on se donne
+        boolean val = false;
+        if () {
+            val = (// on test si ils se chevauchent horizentalement
+            (this.getX() >= item.getX() && this.getX() <= item.getX() + item.getWidth())
                     ||
-                    (this.getX() + this.getWidth()>=item.getX() && this.getX()+this.getWidth()<= item.getX()+item.getWidth())
-                )&&
-                // condition vertical depends du type
-                Math.abs(this.getY() - item.getY())<epsilone;
-            if(val){
+                    (this.getX() + this.getWidth() >= item.getX()
+                            && this.getX() + this.getWidth() <= item.getX() + item.getWidth()))
+                    &&
+                    // condition vertical depends du type
+                    Math.abs(this.getY() - item.getY()) < epsilone;
+            if (val) {
                 item.runEffect(this);
             }
 
-        }else if(go instanceof Plateforme plateforme){
-            val =(// on test si ils se chevauchent horizentalement  
-                    (this.getX() >=plateforme.getX() && this.getX()<= plateforme.getX()+plateforme.getWidth())
+        } else if (go.getClass().equals(this)) {
+            Plateforme plateforme = (Plateforme) go;
+            val = (// on test si ils se chevauchent horizentalement
+            (this.getX() >= plateforme.getX() && this.getX() <= plateforme.getX() + plateforme.getWidth())
                     ||
-                    (this.getX() + this.getWidth()>=plateforme.getX() && this.getX()+this.getWidth()<= plateforme.getX()+plateforme.getWidth())
-                )&&
-                // condition vertical depends du type 
-                Math.abs(this.getY() - plateforme.getY()+plateforme.getHeight())<epsilone
-                && 
-                dy<=0 // le personnage decends
-                ;
-                if(val){
-                    dy=plateforme.saut;
-                }
+                    (this.getX() + this.getWidth() >= plateforme.getX()
+                            && this.getX() + this.getWidth() <= plateforme.getX() + plateforme.getWidth()))
+                    &&
+                    // condition vertical depends du type
+                    Math.abs(this.getY() - plateforme.getY() + plateforme.getHeight()) < epsilone
+                    &&
+                    dy <= 0 // le personnage decends
+            ;
+            if (val) {
+                dy = plateforme.saut;
             }
+        }
         return val;
     }
-
 
     public double getDx() {
         return dx;
     }
 
-
     public void setDx(double dx) {
         this.dx = dx;
     }
-
 
     public double getDy() {
         return dy;
     }
 
-
     public void setDy(double dy) {
         this.dy = dy;
     }
-
-
-
-
-
-
 
 }
--- src/gameobjects/Terrain.java
+++ src/gameobjects/Terrain.java
@@ -3,30 +3,34 @@ package gameobjects;
 import java.util.ArrayList;
 import java.util.Random;
 
+import gameobjects.MovingPlateforme;
+
 public class Terrain {
 
     ArrayList<GameObject> objetcs;
     Joueur joueur;
-    final double height,width;// dimensions du terrain
-    double y=0;// hauteur du jeu. On l'utilisera aussi pour le score
-    
-    /** entre 0 et 1, indique de combien on fais monter le jeu
+    final double height, width;// dimensions du terrain
+    double y = 0;// hauteur du jeu. On l'utilisera aussi pour le score
+
+    /**
+     * entre 0 et 1, indique de combien on fais monter le jeu
      */
-    double advancement=0.3;
+    double advancement = 0.3;
 
-    /** Hauteur maximal que peut attendre le personnage, maxHeight*height, avant de faire monter le jeu. 
+    /**
+     * Hauteur maximal que peut attendre le personnage, maxHeight*height, avant de
+     * faire monter le jeu.
      */
-    double maxHeight=0.8;
+    double maxHeight = 0.8;
 
+    /**
+     * baisse le plus le score monte, affecte la densite des plateformes et la proba
+     * qu'un heli ou fusee apare,
+     * inversement affacte la proba d'apparition des monstres, surement on inverse
+     * 1/difficulte
+     */
+    double difficulty = 1.0;
 
-    /** baisse le plus le score monte, affecte la densite des plateformes et la proba qu'un heli ou fusee apare, 
-     *  inversement affacte la proba d'apparition des monstres, surement on inverse 1/difficulte 
-    */
-    double difficulty=1.0;
-    
-    
-    
-    
     public Terrain(Joueur joueur, double height, double width) {
         this.objetcs = new ArrayList<GameObject>();
         this.joueur = joueur;
@@ -38,25 +42,26 @@ public class Terrain {
      * @param nb nombres d'obstacles a generer
      * 
      */
-    void generateObstacles(int nb){
-        nb = (int)(nb*difficulty);
+    void generateObstacles(int nb) {
+        nb = (int) (nb * difficulty);
         for (int i = 0; i < nb; i++) {
-            Random rand=new Random();
-            objetcs.add(new PlateformeBase(rand.nextDouble(this.width), rand.nextDouble(this.height+y), 
-                                           -1,-1,-1 ));
-                                        //    TODO: combien pour saut? height et width             
+            Random rand = new Random();
+            objetcs.add(new PlateformeBase(rand.nextDouble(this.width), rand.nextDouble(this.height + y),
+                    -1, -1, -1));
+            // TODO: combien pour saut? height et width
         }
-                    
+
     }
 
     /**
      * 
-     * @param minY indique la coordonnee minimal y que peut avoir un objets, si en dessous on l'enleve
+     * @param minY indique la coordonnee minimal y que peut avoir un objets, si en
+     *             dessous on l'enleve
      */
-    void removeObstacles(double minY){
-        int c=0;//compte le nombres d'obstacles qu'on enleve
+    void removeObstacles(double minY) {
+        int c = 0;// compte le nombres d'obstacles qu'on enleve
         for (GameObject gameObject : objetcs) {
-            if(gameObject.getY()<=minY){
+            if (gameObject.getY() <= minY) {
                 objetcs.remove(gameObject);
                 ++c;
             }
@@ -64,23 +69,23 @@ public class Terrain {
         // si on veut des nouveaux obstacles.
         // generateObstacles(c);
     }
-    
-    void endGame(){
-        //TODO: gerer la fin du jeu
-    }
- 
 
-    void update(double deltaT){
+    void endGame() {
+        // TODO: gerer la fin du jeu
+    }
+
+    void update(double deltaT) {
         joueur.perso.move(deltaT);
         limite(joueur.perso);
 
-        if(joueur.perso.getY()<=0){
+        if (joueur.perso.getY() <= 0) {
             endGame();
-        }else if(joueur.perso.getY()+joueur.perso.getHeight()>maxHeight*height){
-            y=height*advancement;
-        }    
+        } else if (joueur.perso.getY() + joueur.perso.getHeight() > maxHeight * height) {
+            y = height * advancement;
+        }
         for (GameObject gameObject : objetcs) {
-            if(gameObject instanceof MovingPlateforme a ){// jsp s'il ya un meilleur moyen pour voir si l'objet est moveable
+            if (gameObject instanceof MovingPlateforme a) {// jsp s'il ya un meilleur moyen pour voir si l'objet est
+                                                           // moveable
                 a.move(deltaT);
                 limite(a);
             }
@@ -89,10 +94,10 @@ public class Terrain {
     }
 
     private void limite(GameObject object) {
-        if(object.getX()+object.getWidth()/2<=0) //bord a droite, on le mets a gauche
-            object.setX(width-object.getWidth()/2);
-        else if (object.getX()+object.getWidth()/2>=width) //bord a gauche, on le mets a gauche
-            object.setX(object.getWidth()/2);
+        if (object.getX() + object.getWidth() / 2 <= 0) // bord a droite, on le mets a gauche
+            object.setX(width - object.getWidth() / 2);
+        else if (object.getX() + object.getWidth() / 2 >= width) // bord a gauche, on le mets a gauche
+            object.setX(object.getWidth() / 2);
     }
 
 }
