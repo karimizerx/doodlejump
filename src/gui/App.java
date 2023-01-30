39dcc6c53c36cb6211aee3d2aae7b504a75a4faf
laidouni <mohamed.laidouni@etu.u-paris.fr>
2023-01-30 16:15:24 +0100

    Derniers Reglages de pb

--- src/gui/App.java
+++ src/gui/App.java
@@ -2,15 +2,10 @@ package gui;
 
 import javax.swing.JFrame;
 
-import gameobjects.Fusee;
-//import gameobjects.Items;
+import gameobjects.*;
 
 public class App extends JFrame {
 
     public static void main(String[] args) {
-        System.out.println("Alooo");
-
-        Fusee fus = new Fusee(0, 0, 0, 0, 0);
-        System.out.println("Le nom de la classe Items est : " + fus.getClass().getName());
     }
 }
