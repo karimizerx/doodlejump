5b773742bfb56bef346002cd4660f27c0cced41d
laidouni <mohamed.laidouni@etu.u-paris.fr>
2023-02-08 00:34:51 +0100

    Nettoyage/Refactoring, v2 du menu avec champs pour le nom du joueur, réglage parfait des coordonnées pour la collision et le débordement

--- src/gui/Vue.java
+++ src/gui/Vue.java
@@ -1,40 +1,161 @@
 package gui;
 
-import javax.swing.*;
+// Import d'autres dossiers
+import gameobjects.*;
 
-import gameobjects.Joueur;
-import gameobjects.Personnage;
-import gameobjects.Plateforme;
-import gameobjects.Terrain;
-
+// Import de packages java
+import java.io.*;
 import java.awt.*;
+import java.awt.image.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import javax.swing.WindowConstants;
-
-import java.awt.image.*;
+import javax.swing.*;
 import javax.imageio.*;
-import java.io.*;
 
+// S'occupe d'afficher les éléments du terrain
 public class Vue extends JPanel implements Runnable, KeyListener {
 
     public static boolean isRunning;
-    Thread thread;
-    BufferedImage view, terrainView, platformeView, persoView, scoreView, scoreBackgroundView;
-
-    Terrain terter;
-    boolean isRight, isLeft, isMenu, isEsc;
-    boolean pause = false;
-    JFrame menuPause;
-    JPanel menuPanel;
-    String chemin = (new File("gui/images/")).getAbsolutePath();
+    private Thread thread;
+    private String chemin = (new File("gui/images/")).getAbsolutePath();
+    private BufferedImage view, terrainView, platformeView, persoView, scoreView, scoreBackgroundView;
+    private boolean isRight, isLeft, pause = false;
+    private Terrain terrain;
+    private JFrame menuPause;
 
     public Vue(Terrain ter) {
-        this.terter = ter;
-        setPreferredSize(new Dimension((int) terter.getWidth(), (int) terter.getHeight()));
-        addKeyListener(this);
+        this.terrain = ter;
+        // Taille du panel
+        this.setPreferredSize(new Dimension((int) terrain.getWidth(), (int) terrain.getHeight()));
+        // Gestion d'évènements boutons
+        this.addKeyListener(this);
     }
 
+    // Méthodes de la classe
+
+    // Initialise toutes les images du jeu
+    private void init() {
+        // view est l'image qui contiendra toutes les autres
+        view = new BufferedImage((int) terrain.getWidth(), (int) terrain.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        // Double try_catch pour gérer la différence entre windows & linux
+        try {
+            try {
+                terrainView = ImageIO.read(new File(chemin + "/" + "background.png"));
+                platformeView = ImageIO.read(new File(chemin + "/" + "plateformeBase.png"));
+                persoView = ImageIO.read(new File(chemin + "/" + "doodleNinja.png"));
+                scoreBackgroundView = ImageIO.read(new File(chemin + "/" + "scoreBackground.png"));
+            } catch (Exception e) {
+                terrainView = ImageIO.read(new File("src/gui/images/background.png"));
+                platformeView = ImageIO.read(new File("src/gui/images/plateforme.png"));
+                persoView = ImageIO.read(new File("src/gui/images/doodleNinja.png"));
+                scoreBackgroundView = ImageIO.read(new File("src/gui/images/scoreBackground.png"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Dessine toutes les images
+    public void draw() {
+        Graphics2D g2 = (Graphics2D) view.getGraphics();
+        g2.drawImage(terrainView, 0, 0, (int) terrain.getWidth(), (int) terrain.getHeight(), null); // Affichage terrain
+
+        // Affichage des plateformes
+        for (Plateforme pf : terrain.getPlateformesListe()) {
+            g2.drawImage(
+                    platformeView,
+                    (int) pf.getX(),
+                    (int) pf.getY(),
+                    (int) pf.getWidth(),
+                    (int) pf.getHeight(),
+                    null);
+        }
+
+        // Affichage du Score
+        // Seulement s'il n'y a qu'un joueur
+        if (terrain.getListeJoueurs().length == 1) {
+            String score = String.valueOf(terrain.getListeJoueurs()[0].getScore());
+            g2.drawImage(scoreBackgroundView, 2, 2, 60 + (30 * (score.length() - 1)), 55, null);
+            for (int i = 0; i < score.length(); ++i) {
+                try {
+                    try {
+                        scoreView = ImageIO.read(new File(chemin + "/ch" + score.charAt(i) + ".png"));
+
+                    } catch (Exception e) {
+                        scoreView = ImageIO.read(new File("src/gui/images/ch" + score.charAt(i) + ".png"));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                g2.drawImage(scoreView, 5 + (25 * i), 5, 50, 50, null);
+            }
+        }
+
+        // Affichage des personnages
+        for (int i = 0; i < terrain.getListeJoueurs().length; ++i) {
+            Joueur j = terrain.getListeJoueurs()[i];
+            Personnage p = j.getPerso();
+            g2.drawImage(persoView, (int) p.getX(), (int) p.getY(), (int) p.getWidth(), (int) p.getHeight(), null);
+        }
+
+        // Affichage final
+        Graphics g = getGraphics(); // Contexte graphique
+        g.drawImage(view, 0, 0, (int) terrain.getWidth(), (int) terrain.getHeight(), null);
+        g.dispose();
+    }
+
+    // Gère le cas de fin du jeu
+    public boolean endGame() {
+        isRunning = false;
+        boolean isFin = false;
+        // Si un joueur à perdu, c'est fini
+        for (int i = 0; i < terrain.getListeJoueurs().length; ++i) {
+            Joueur j = terrain.getListeJoueurs()[i];
+            if (j.getPerso().getY() + j.getPerso().getHeight() > this.getHeight()) {
+                isFin = true;
+            }
+        }
+        return isFin;
+    }
+
+    // Met à jour l'affichage
+    public void update() {
+        for (int i = 0; i < terrain.getListeJoueurs().length; ++i) {
+            Joueur j = terrain.getListeJoueurs()[i];
+            Personnage p = j.getPerso();
+            // Gère les boutons flèches
+            if (isRight) {
+                p.setX(p.getX() + 5);
+            } else if (isLeft) {
+                p.setX(p.getX() - 5);
+            }
+        }
+        terrain.update();
+    }
+
+    // Fait tourner le jeu
+    @Override
+    public void run() {
+        try {
+            requestFocusInWindow();
+            init();
+            while (isRunning) {
+                if (!pause)
+                    update();
+                draw();
+                Thread.sleep(10); // Temps de stop d'update, en ms
+            }
+            if (endGame()) { // Si c'est la fin du jeu, on retire tout
+                removeAll();
+                repaint();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // ????
     @Override
     public void addNotify() {
         super.addNotify();
@@ -45,181 +166,59 @@ public class Vue extends JPanel implements Runnable, KeyListener {
         }
     }
 
-    public void start() {
-        try {
-            try {
-                view = new BufferedImage((int) terter.getWidth(), (int) terter.getHeight(), BufferedImage.TYPE_INT_RGB);
-
-                terrainView = ImageIO.read(new File(chemin + "/" + "background.png"));
-                platformeView = ImageIO.read(new File(chemin + "/" + "plateformeBase.png"));
-                persoView = ImageIO.read(new File(chemin + "/" + "doodleNinja.png"));
-                scoreBackgroundView = ImageIO.read(new File(chemin + "/" + "scoreBackground.png"));
-
-            } catch (Exception e) {
-                terrainView = ImageIO.read(new File("src/gui/images/background.png"));
-                platformeView = ImageIO.read(new File("src/gui/images/plateforme.png"));
-                persoView = ImageIO.read(new File("src/gui/images/doodleNinja.png"));
-                scoreBackgroundView = ImageIO.read(new File("src/gui/images/scoreBackground.png"));
-
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public boolean endGame() {
-        isRunning = false;
-        boolean isFin = false;
-        for (int i = 0; i < terter.getListeJoueurs().length; ++i) {
-            Joueur j = terter.getListeJoueurs()[i];
-            if (j.getPerso().getY() + j.getPerso().getHeight() > this.getHeight()) {
-                isFin = true;
-            }
-        }
-        return isFin;
-    }
-
-    public void update() {
-
-        for (int i = 0; i < terter.getListeJoueurs().length; ++i) {
-            Joueur j = terter.getListeJoueurs()[i];
-            Personnage p = j.getPerso();
-            // Gère les boutons flèches
-            if (isRight) {
-                p.setX(p.getX() + 5);
-            } else if (isLeft) {
-                p.setX(p.getX() - 5);
-            }
-        }
-
-        terter.update();
-    }
-
-    public void draw() {
-
-        Graphics2D g2 = (Graphics2D) view.getGraphics();
-        g2.drawImage(terrainView, 0, 0, (int) terter.getWidth(), (int) terter.getHeight(), null);
-
-        // Affichage des plateformes
-        for (Plateforme pf : terter.getPlateformesListe()) {
-            g2.drawImage(
-                    platformeView,
-                    (int) pf.getX(),
-                    (int) pf.getY(),
-                    (int) pf.getWidth(),
-                    (int) pf.getHeight(),
-                    null);
-        }
-
-        /// Affichage du Score
-        if (terter.getListeJoueurs().length == 1) {
-            String score = String.valueOf(terter.getListeJoueurs()[0].getScore());
-            g2.drawImage(scoreBackgroundView, 2, 2, 60 + (30 * (score.length() - 1)), 55, null);
-            for (int i = 0; i < score.length(); ++i) {
-                char chiffre = score.charAt(i);
-                try {
-                    try {
-                        scoreView = ImageIO.read(new File(chemin + "/ch" + chiffre + ".png"));
-
-                    } catch (Exception e) {
-                        scoreView = ImageIO.read(new File("src/gui/images/ch" + chiffre + ".png"));
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                g2.drawImage(scoreView, 5 + (25 * i), 5, 50, 50, null);
-            }
-        }
-
-        for (int i = 0; i < terter.getListeJoueurs().length; ++i) {
-            Joueur j = terter.getListeJoueurs()[i];
-            Personnage p = j.getPerso();
-            g2.drawImage(persoView, (int) p.getX(), (int) p.getY(), (int) p.getWidth(), (int) p.getHeight(), null);
-        }
-
-        Graphics g = getGraphics();
-        g.drawImage(view, 0, 0, (int) terter.getWidth(), (int) terter.getHeight(), null);
-        g.dispose();
-    }
-
-    @Override
-    public void run() {
-        try {
-            requestFocusInWindow();
-            start();
-            while (isRunning) {
-                if (!pause)
-                    update();
-                draw();
-                Thread.sleep(10);
-            }
-            if (endGame()) {
-                removeAll();
-                repaint();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     // Gestion des boutons
-    @Override
-    public void keyTyped(KeyEvent e) {
 
-    }
+    private void pause() {
+        this.pause = !this.pause;
+        this.menuPause = new JFrame();
+        this.menuPause.setBounds((int) terrain.getWidth() * 3 / 2 - 50, (int) terrain.getHeight() / 2 - 60, 150, 120);
+        this.menuPause.setResizable(false);
+        this.menuPause.setLayout(new FlowLayout());
+        this.menuPause.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            isRight = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            isLeft = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-            pause();
-        }
-    }
+        JButton cont = new JButton("Continuer");
+        JButton exit = new JButton("Menu principal");
 
-    void pause() {
-        isEsc = true;
-        pause = !pause;
-        menuPause = new JFrame();
-        menuPause.setBounds((int) terter.getWidth() * 3 / 2 - 50, (int) terter.getHeight() / 2 - 60, 100, 120);
-        menuPause.setResizable(false);
-        menuPause.setLayout(new FlowLayout());
-        menuPause.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-
-        JButton cont = new JButton("Continue");
-        JButton exit = new JButton("Exit");
-
-        menuPause.add(cont);
-        menuPause.add(exit);
-        menuPause.setVisible(true);
+        this.menuPause.add(cont);
+        this.menuPause.add(exit);
+        this.menuPause.setVisible(true);
 
         cont.addActionListener(ev -> {
-            menuPause.dispose();
-            pause = !pause;
-
+            this.menuPause.dispose();
+            this.pause = !this.pause;
         });
 
         exit.addActionListener(ev -> {
-            menuPause.dispose();
+            this.menuPause.dispose();
             JFrame retourMenu = new App();
             retourMenu.setVisible(true);
         });
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            isRight = false;
+            this.isRight = true;
         }
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            isLeft = false;
+            this.isLeft = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            pause();
         }
     }
 
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            this.isRight = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            this.isLeft = false;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
 }
